8483fa220e41d2878ca12505d4066d907336a1e1
Nywton Barros <nywton.barros@gmail.com>
2025-07-14 21:22:29 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { GildedRose } from './gilded-rose';
+import { Item } from './item';
+
+const items: Array<Item> = []
+
+items.push(new Item('+5 Dexterity Vest', 10, 20));
+items.push(new Item('Aged Brie', 2, 0));
+items.push(new Item('Elixir of the Mongoose', 5, 7));
+items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
+items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+items.push(new Item('Conjured Mana Cake', 3, 6));
+
+const gildedRose = new GildedRose(items);
+
+gildedRose.updateQuality();
